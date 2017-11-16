d8f84224ab7215bf7983b511b06f58a9fb92cb1e
shun1249844726 <1249844726@qq.com>
2017-11-16 16:09:19 +0800

    Initial commit

---
+++ app/src/main/java/com/lexinsmart/xushun/hotfixdemo/MainApplication.java
@@ -0,0 +1,69 @@
+package com.lexinsmart.xushun.hotfixdemo;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import com.taobao.sophix.PatchStatus;
+import com.taobao.sophix.SophixManager;
+import com.taobao.sophix.listener.PatchLoadStatusListener;
+
+/**
+ * User: xingzhi.wap
+ * Date:16/5/17
+ */
+public class MainApplication extends Application {
+    public interface MsgDisplayListener {
+        void handle(String msg);
+    }
+
+    public static MsgDisplayListener msgDisplayListener = null;
+    public static StringBuilder cacheMsg = new StringBuilder();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initHotfix();
+
+    }
+
+    private void initHotfix() {
+        String appVersion;
+        try {
+            appVersion = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+            System.out.println("version:"+appVersion);
+        } catch (Exception e) {
+            appVersion = "1.0.0";
+        }
+
+        SophixManager.getInstance().setContext(this)
+                .setAppVersion(appVersion)
+                .setAesKey(null)
+                //.setAesKey("0123456789123456")
+                .setEnableDebug(true)
+                .setPatchLoadStatusStub(new PatchLoadStatusListener() {
+                    @Override
+                    public void onLoad(final int mode, final int code, final String info, final int handlePatchVersion) {
+                        String msg = new StringBuilder("").append("Mode:").append(mode)
+                                .append(" \nCode:").append(code)
+                                .append(" \nInfo:").append(info)
+                                .append(" \nHandlePatchVersion:").append(handlePatchVersion).toString();
+                        if (msgDisplayListener != null) {
+                            msgDisplayListener.handle(msg);
+                        } else {
+                            cacheMsg.append("\n").append(msg);
+                        }
+
+                        // 补丁加载回调通知
+                        if (code == PatchStatus.CODE_LOAD_SUCCESS) {
+                            // 表明补丁加载成功
+                            System.out.println("补丁加载ok.!");
+                        } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
+                            // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
+                            // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
+                        } else {
+                            // 其它错误信息, 查看PatchStatus类说明
+                        }
+                    }
+                }).initialize();
+    }
+}
